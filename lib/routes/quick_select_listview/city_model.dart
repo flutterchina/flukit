f4dced34d41c392af320bd676eea6289199e7a69
duwen <duwen-pbj@sinosig.com>
2018-09-26 18:39:21 +0800

    update

---
+++ lib/routes/quick_select_listview/city_model.dart
@@ -0,0 +1,29 @@
+import 'package:flukit/flukit.dart';
+
+class CityInfo extends ISuspensionBean {
+  String name;
+  String tagIndex;
+  String namePinyin;
+
+  CityInfo({
+    this.name,
+    this.tagIndex,
+    this.namePinyin,
+  });
+
+  CityInfo.fromJson(Map<String, dynamic> json)
+      : name = json['name'] == null ? "" : json['name'];
+
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'tagIndex': tagIndex,
+    'namePinyin': namePinyin,
+    'isShowSuspension': isShowSuspension
+  };
+
+  @override
+  String getSuspensionTag() => tagIndex;
+
+  @override
+  String toString() => "CityBean {" + " \"name\":\"" + name + "\"" + '}';
+}
